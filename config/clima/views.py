e86c44fc8227347794bf91dcf9518bdc072e2640
TizianoAl <alfazaktiziano@gmail.com>
2020-10-27 17:05:04 -0300

    Cambios en la view y html

--- config/clima/views.py
+++ config/clima/views.py
@@ -1,4 +1,18 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'clima/clima.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=333079c49b302f10109f0cdea2cedf1a'
+    ciudad = 'London'
+
+    r = requests.get(url.format(ciudad)).json()
+
+    clima_ciudad = {
+    'ciudad' : ciudad,
+    'temperatura' : r['main']['temp'],
+    'descripcion' : r['weather'][0]['description'],
+    'icono' : r['weather'][0]['icon'],
+    }
+
+    context = {'clima_ciudad' : clima_ciudad}
+    return render(request, 'clima/clima.html', context)
