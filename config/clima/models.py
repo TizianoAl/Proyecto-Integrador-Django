9dbcfcbb276ab18500fade63594195408bd6d3d5
TizianoAl <alfazaktiziano@gmail.com>
2020-10-29 19:45:09 -0300

    Adicion de base de datos

--- config/clima/models.py
+++ config/clima/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = 'ciudades'
--- config/clima/views.py
+++ config/clima/views.py
@@ -1,18 +1,28 @@
 import requests
 from django.shortcuts import render
+from .models import Ciudad
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=333079c49b302f10109f0cdea2cedf1a&lang=es'
     ciudad = 'London'
 
-    r = requests.get(url.format(ciudad)).json()
+    ciudades = Ciudad.objects.all()
 
-    clima_ciudad = {
-    'ciudad' : ciudad,
-    'temperatura' : r['main']['temp'],
-    'descripcion' : r['weather'][0]['description'],
-    'icono' : r['weather'][0]['icon'],
-    }
+    clima_data = []
 
-    context = {'clima_ciudad' : clima_ciudad}
+    for ciudad in ciudades:
+
+        r = requests.get(url.format(ciudad)).json()
+
+        clima_ciudad = {
+        'ciudad' : ciudad.nombre,
+        'temperatura' : r['main']['temp'],
+        'descripcion' : r['weather'][0]['description'],
+        'icono' : r['weather'][0]['icon'],
+        }
+
+        clima_data.append(clima_ciudad)
+
+
+    context = {'clima_data' : clima_data}
     return render(request, 'clima/clima.html', context)
