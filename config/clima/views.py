067caca45f7c1feadc4643cb01cf7ef3aa4d3b06
TizianoAl <alfazaktiziano@gmail.com>
2020-11-03 15:48:12 -0300

    Adicion de alertas

--- config/clima/views.py
+++ config/clima/views.py
@@ -6,9 +6,34 @@ from .forms import CiudadForm
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=333079c49b302f10109f0cdea2cedf1a&lang=es'
 
+    err_msg = ''
+    mensaje = ''
+    mensaje_class = ''
+
     if request.method == 'POST':
         form = CiudadForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            nueva_ciudad = form.cleaned_data['nombre']
+            existe_ciudad = Ciudad.objects.filter(nombre=nueva_ciudad).count()
+
+            if existe_ciudad == 0:
+                r = requests.get(url.format(nueva_ciudad)).json()
+
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'La ciuda que desea ingresar no existe'
+            else:
+                err_msg = 'La ciudad que desea ingresar ya existe en la base de datos'
+
+        if err_msg:
+            mensaje = err_msg
+            mensaje_class = 'is-danger'
+        else:
+            mensaje = 'La ciudad se agregó correctamente'
+            mensaje_class = 'is-success'
+
 
     form = CiudadForm()
 
@@ -30,5 +55,10 @@ def index(request):
         clima_data.append(clima_ciudad)
 
 
-    context = {'clima_data' : clima_data, 'form' : form}
+    context = {
+        'clima_data' : clima_data,
+        'form' : form,
+        'mensaje' : mensaje,
+        'mensaje_class' : mensaje_class,
+    }
     return render(request, 'clima/clima.html', context)
